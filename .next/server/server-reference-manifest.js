39ef3a5697e7d1d40ac7359daae0534ab9daf50b
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2024-08-02 15:41:19 -0500

    proyecto terminado correccion de menu hamburguer

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"m882IZ7BtyLwD8Drv02MZx2n2mWJn6r6LwKnVuIcOa4=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"mUYPqqSUlEjtxqQsdIQq9gsNqpiKYrRLnwW7g/bFGDA=\"}"
